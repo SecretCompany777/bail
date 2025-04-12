3162f7f0a589deba9b7697ecea724f66cf658fe3
Secret Company <secretcompany70@gmail.com>
2025-04-12 10:34:11 +0800

    Update lt-hash.ts

--- src/Utils/lt-hash.ts
+++ src/Utils/lt-hash.ts
@@ -10,52 +10,68 @@ const o = 128
 
 class d {
 
-	salt: string
+    salt: string
 
-	constructor(e: string) {
-		this.salt = e
-	}
-	add(e, t) {
-		var r = this
-		for(const item of t) {
-			e = r._addSingle(e, item)
-		}
+    constructor(e: string) {
+        this.salt = e
+    }
 
-		return e
-	}
-	subtract(e, t) {
-		var r = this
-		for(const item of t) {
-			e = r._subtractSingle(e, item)
-		}
+    add(e, t) {
+        var r = this
+        for(const item of t) {
+            e = r._addSingle(e, item)
+        }
 
-		return e
-	}
-	subtractThenAdd(e, t, r) {
-		var n = this
-		return n.add(n.subtract(e, r), t)
-	}
-	_addSingle(e, t) {
-		var r = this
-		const n = new Uint8Array(hkdf(Buffer.from(t), o, { info: r.salt })).buffer
-		return r.performPointwiseWithOverflow(e, n, ((e, t) => e + t))
-	}
-	_subtractSingle(e, t) {
-		var r = this
+        return e
+    }
 
-		const n = new Uint8Array(hkdf(Buffer.from(t), o, { info: r.salt })).buffer
-		return r.performPointwiseWithOverflow(e, n, ((e, t) => e - t))
-	}
-	performPointwiseWithOverflow(e, t, r) {
-		const n = new DataView(e)
-		  , i = new DataView(t)
-		  , a = new ArrayBuffer(n.byteLength)
-		  , s = new DataView(a)
-		for(let e = 0; e < n.byteLength; e += 2) {
-			s.setUint16(e, r(n.getUint16(e, !0), i.getUint16(e, !0)), !0)
-		}
+    subtract(e, t) {
+        var r = this
+        for(const item of t) {
+            e = r._subtractSingle(e, item)
+        }
 
-		return a
-	}
+        return e
+    }
+
+    subtractThenAdd(e, t, r) {
+        var n = this
+        return n.add(n.subtract(e, r), t)
+    }
+
+    _addSingle(e, t) {
+        var r = this
+        const n = new Uint8Array(hkdf(Buffer.from(t), o, { info: r.salt })).buffer
+        return r.performPointwiseWithOverflow(e, n, ((e, t) => e + t))
+    }
+
+    _subtractSingle(e, t) {
+        var r = this
+
+        const n = new Uint8Array(hkdf(Buffer.from(t), o, { info: r.salt })).buffer
+        return r.performPointwiseWithOverflow(e, n, ((e, t) => e - t))
+    }
+
+    performPointwiseWithOverflow(e, t, r) {
+        // Ensure that both e and t are ArrayBuffers
+        if (!(e instanceof ArrayBuffer)) {
+            throw new TypeError("First argument to DataView constructor must be an ArrayBuffer");
+        }
+        if (!(t instanceof ArrayBuffer)) {
+            throw new TypeError("Second argument to DataView constructor must be an ArrayBuffer");
+        }
+
+        const n = new DataView(e),
+              i = new DataView(t),
+              a = new ArrayBuffer(n.byteLength),
+              s = new DataView(a);
+
+        for (let e = 0; e < n.byteLength; e += 2) {
+            s.setUint16(e, r(n.getUint16(e, !0), i.getUint16(e, !0)), !0);
+        }
+
+        return a
+    }
 }
+
 export const LT_HASH_ANTI_TAMPERING = new d('WhatsApp Patch Integrity')
